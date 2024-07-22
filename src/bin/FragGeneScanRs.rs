5d4f2225b5fccce411f896c7cb67512044ffca9d
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2024-07-21 21:31:52 -0400

    Update command line parsing
    update clap to 4.5

--- src/bin/FragGeneScanRs.rs
+++ src/bin/FragGeneScanRs.rs
@@ -7,165 +7,163 @@ use std::io;
 use std::io::{Read, Write};
 use std::path::PathBuf;
 use std::sync::Mutex;
-
-extern crate anyhow;
-use anyhow::Result;
-
-extern crate clap;
-use clap::{crate_version, App, Arg};
-
-extern crate seq_io;
+use clap::{Arg, Command};
 use seq_io::fasta;
-
-extern crate rayon;
 use rayon::iter::{ParallelBridge, ParallelIterator};
 use rayon::ThreadPoolBuilder;
-
-extern crate frag_gene_scan_rs;
-use frag_gene_scan_rs::dna::Nuc;
-use frag_gene_scan_rs::gene;
+use frag_gene_scan_rs::dna::{count_cg_content, Nuc};
 use frag_gene_scan_rs::hmm;
 use frag_gene_scan_rs::viterbi::viterbi;
+use anyhow::Result;
+use env_logger::Builder;
+
+pub fn init_log() -> u64 {
+    Builder::from_default_env().init();
+    println!("\n ************** initializing logger *****************\n");
+    return 1;
+}
 
 fn main() -> Result<()> {
-    let matches = App::new("FragGeneScanRs")
-        .version(crate_version!())
+    let _ = init_log();
+    let matches = Command::new("FragGeneScanRs")
+        .version("0.0.1")
         .author("Felix Van der Jeugt <felix.vanderjeugt@ugent.be>")
         .about("Scalable high-throughput short-read open reading frame prediction.")
-        .arg(Arg::with_name("seq-file")
-            .short("s")
-            .long("seq-file-name")
-            .value_name("seq_file_name")
-            .takes_value(true)
-            .default_value("stdin")
-            .help("Sequence file name including the full path. Using 'stdin' (or not suplying this argument) reads from standard input."))
-        .arg(Arg::with_name("output-prefix")
-            .short("o")
-            .long("output-prefix")
-            .value_name("output_prefix")
-            .takes_value(true)
-            .help("Output metadata (.out and .gff), proteins (.faa) and genes (.ffn) to files with this prefix. Use 'stdout' to write the predicted proteins to standard output."))
-        .arg(Arg::with_name("complete")
-            .short("w")
-            .long("complete")
-            .value_name("complete")
-            .takes_value(true)
-            .default_value("0")
-            .help("The input sequence has complete genomic sequences; not short sequence reads."))
-        .arg(Arg::with_name("formatted")
-            .short("f")
-            .long("formatted")
-            .help("Format the DNA output."))
-        .arg(Arg::with_name("train-file")
-            .short("t")
-            .long("training-file")
-            .value_name("train_file_name")
-            .takes_value(true)
-            .required(true)
-            .help("File name that contains model parameters; this file should be in the -r directory or one of the following:
-[complete] for complete genomic sequences or short sequence reads without sequencing error
-[sanger_5] for Sanger sequencing reads with about 0.5% error rate
-[sanger_10] for Sanger sequencing reads with about 1% error rate
-[454_5] for 454 pyrosequencing reads with about 0.5% error rate
-[454_10] for 454 pyrosequencing reads with about 1% error rate
-[454_30] for 454 pyrosequencing reads with about 3% error rate
-[illumina_1] for Illumina sequencing reads with about 0.1% error rate
-[illumina_5] for Illumina sequencing reads with about 0.5% error rate
-[illumina_10] for Illumina sequencing reads with about 1% error rate"))
-        .arg(Arg::with_name("train-file-dir")
-            .short("r")
-            .long("train-file-dir")
-            .value_name("train_file_dir")
-            .takes_value(true)
-            .help("Full path of the directory containing the training model files."))
-        .arg(Arg::with_name("thread-num")
-            .short("p")
-            .long("thread-num")
-            .value_name("thread_num")
-            .takes_value(true)
-            .default_value("1")
-            .help("The number of threads used by FragGeneScan++."))
-        .arg(Arg::with_name("meta-file")
-            .short("m")
-            .long("meta-file")
-            .value_name("meta_file")
-            .takes_value(true)
-            .help("Output metadata to this file (supersedes -o)."))
-        .arg(Arg::with_name("gff-file")
-            .short("g")
-            .long("gff-file")
-            .value_name("gff_file")
-            .takes_value(true)
-            .help("Output metadata to this gff formatted file (supersedes -o)."))
-        .arg(Arg::with_name("aa-file")
-            .short("a")
-            .long("aa-file")
-            .value_name("aa_file")
-            .takes_value(true)
-            .help("Output predicted proteins to this file (supersedes -o)."))
-        .arg(Arg::with_name("nucleotide-file")
-            .short("n")
-            .long("nucleotide-file")
-            .value_name("nucleotide_file")
-            .takes_value(true)
-            .help("Output predicted genes to this file (supersedes -o)."))
-        .arg(Arg::with_name("unordered")
-            .short("u")
-            .long("unordered")
-            .help("Do not preserve record order in output (faster)."))
+        .arg(Arg::new("seq-file")
+             .short('s')
+             .long("seq-file-name")
+             .value_name("seq_file_name")
+             .default_value("stdin")
+             .help("Sequence file name including the full path. Using 'stdin' (or not supplying this argument) reads from standard input.")
+             .action(clap::ArgAction::Set))
+        .arg(Arg::new("output-prefix")
+             .short('o')
+             .long("output-prefix")
+             .value_name("output_prefix")
+             .help("Output metadata (.out and .gff), proteins (.faa) and genes (.ffn) to files with this prefix. Use 'stdout' to write the predicted proteins to standard output.")
+             .action(clap::ArgAction::Set))
+        .arg(Arg::new("complete")
+             .short('w')
+             .long("complete")
+             .value_name("complete")
+             .default_value("0")
+             .help("The input sequence has complete genomic sequences; not short sequence reads.")
+             .action(clap::ArgAction::Set))
+        .arg(Arg::new("formatted")
+             .short('f')
+             .long("formatted")
+             .help("Format the DNA output.")
+             .action(clap::ArgAction::SetTrue))
+        .arg(Arg::new("train-file")
+             .short('t')
+             .long("training-file")
+             .value_name("train_file_name")
+             .required(true)
+             .help("File name that contains model parameters; this file should be in the -r directory or one of the following:
+             [complete] for complete genomic sequences or short sequence reads without sequencing error\n
+             [sanger_5] for Sanger sequencing reads with about 0.5% error rate
+             [sanger_10] for Sanger sequencing reads with about 1% error rate
+             [454_5] for 454 pyrosequencing reads with about 0.5% error rate
+             [454_10] for 454 pyrosequencing reads with about 1% error rate
+             [454_30] for 454 pyrosequencing reads with about 3% error rate
+             [illumina_1] for Illumina sequencing reads with about 0.1% error rate
+             [illumina_5] for Illumina sequencing reads with about 0.5% error rate
+             [illumina_10] for Illumina sequencing reads with about 1% error rate")
+             .action(clap::ArgAction::Set))
+        .arg(Arg::new("train-file-dir")
+             .short('r')
+             .long("train-file-dir")
+             .value_name("train_file_dir")
+             .help("Full path of the directory containing the training model files.")
+             .action(clap::ArgAction::Set))
+        .arg(Arg::new("thread-num")
+             .short('p')
+             .long("thread-num")
+             .value_name("thread_num")
+             .default_value("1")
+             .help("The number of threads used by FragGeneScan++.")
+             .action(clap::ArgAction::Set))
+        .arg(Arg::new("meta-file")
+             .short('m')
+             .long("meta-file")
+             .value_name("meta_file")
+             .help("Output metadata to this file (supersedes -o).")
+             .action(clap::ArgAction::Set))
+        .arg(Arg::new("gff-file")
+             .short('g')
+             .long("gff-file")
+             .value_name("gff_file")
+             .help("Output metadata to this gff formatted file (supersedes -o).")
+             .action(clap::ArgAction::Set))
+        .arg(Arg::new("aa-file")
+             .short('a')
+             .long("aa-file")
+             .value_name("aa_file")
+             .help("Output predicted proteins to this file (supersedes -o).")
+             .action(clap::ArgAction::Set))
+        .arg(Arg::new("nucleotide-file")
+             .short('n')
+             .long("nucleotide-file")
+             .value_name("nucleotide_file")
+             .help("Output predicted genes to this file (supersedes -o).")
+             .action(clap::ArgAction::Set))
+        .arg(Arg::new("unordered")
+             .short('u')
+             .long("unordered")
+             .help("Do not preserve record order in output (faster).")
+             .action(clap::ArgAction::SetTrue))
         .get_matches();
 
     let (global, locals) = hmm::get_train_from_file(
-        PathBuf::from(matches.value_of("train-file-dir").unwrap_or("train")),
-        PathBuf::from(matches.value_of("train-file").unwrap()),
+        PathBuf::from(matches.get_one::<String>("train-file-dir").unwrap_or(&"train".to_string())),
+        PathBuf::from(matches.get_one::<String>("train-file").unwrap()),
     )?;
 
-    let inputseqs: Box<dyn Read + Send> = match matches.value_of("seq-file").unwrap_or("stdin") {
+    let input_file = matches.get_one::<String>("seq-file").map(String::as_str).unwrap_or("stdin");
+    let input_seqs: Box<dyn Read + Send> = match input_file {
         "stdin" => Box::new(io::stdin()),
         filename => Box::new(File::open(filename)?),
     };
 
+    let output_prefix = matches.get_one::<String>("output-prefix").map(String::as_str);
+
     let mut aastream: Option<Box<dyn Write + Send>> = match (
-        matches.value_of("aa-file"),
-        matches.value_of("output-prefix"),
+        matches.get_one::<String>("aa-file").map(String::as_str),
+        output_prefix
     ) {
         (Some(filename), _) => Some(Box::new(File::create(filename)?)),
         (None, Some("stdout")) => Some(Box::new(io::stdout())),
-        (None, Some(filename)) => Some(Box::new(File::create(filename.to_owned() + ".faa")?)),
+        (None, Some(prefix)) => Some(Box::new(File::create(format!("{}.faa", prefix))?)),
         (None, None) => None,
     };
 
     let metastream: Option<Box<dyn Write + Send>> = match (
-        matches.value_of("meta-file"),
-        matches.value_of("output-prefix"),
+        matches.get_one::<String>("meta-file").map(String::as_str),
+        output_prefix
     ) {
         (Some(filename), _) => Some(Box::new(File::create(filename)?)),
         (None, Some("stdout")) => None,
-        (None, Some(filename)) => Some(Box::new(File::create(filename.to_owned() + ".out")?)),
+        (None, Some(prefix)) => Some(Box::new(File::create(format!("{}.out", prefix))?)),
         (None, None) => None,
     };
 
-    let mut gffstream: Option<Box<dyn Write + Send>> = match (
-        matches.value_of("gff-file"),
-        matches.value_of("output-prefix"),
+    let gffstream: Option<Box<dyn Write + Send>> = match (
+        matches.get_one::<String>("gff-file").map(String::as_str),
+        output_prefix
     ) {
         (Some(filename), _) => Some(Box::new(File::create(filename)?)),
         (None, Some("stdout")) => None,
-        (None, Some(filename)) => Some(Box::new(File::create(filename.to_owned() + ".gff")?)),
+        (None, Some(prefix)) => Some(Box::new(File::create(format!("{}.gff", prefix))?)),
         (None, None) => None,
     };
-
-    if let Some(ref mut gff) = &mut gffstream {
-        writeln!(gff, "##gff-version 3")?;
-    }
 
     let dnastream: Option<Box<dyn Write + Send>> = match (
-        matches.value_of("nucleotide-file"),
-        matches.value_of("output-prefix"),
+        matches.get_one::<String>("nucleotide-file").map(String::as_str),
+        output_prefix
     ) {
         (Some(filename), _) => Some(Box::new(File::create(filename)?)),
         (None, Some("stdout")) => None,
-        (None, Some(filename)) => Some(Box::new(File::create(filename.to_owned() + ".ffn")?)),
+        (None, Some(prefix)) => Some(Box::new(File::create(format!("{}.ffn", prefix))?)),
         (None, None) => None,
     };
 
@@ -173,31 +171,31 @@ fn main() -> Result<()> {
         aastream = Some(Box::new(io::stdout()));
     }
 
-    if matches.is_present("unordered") {
+    if matches.contains_id("unordered") {
         run(
             global,
             locals,
-            inputseqs,
+            input_seqs,
             aastream.map(UnbufferingBuffer::new),
             metastream.map(UnbufferingBuffer::new),
             gffstream.map(UnbufferingBuffer::new),
             dnastream.map(UnbufferingBuffer::new),
-            matches.value_of("complete").unwrap() == "1",
-            matches.is_present("formatted"),
-            usize::from_str_radix(matches.value_of("thread-num").unwrap(), 10)?,
+            matches.get_one::<String>("complete").map(|s| s == "1").unwrap_or(false),
+            matches.contains_id("formatted"),
+            matches.get_one::<String>("thread-num").and_then(|s| s.parse::<usize>().ok()).unwrap_or(1),
         )?;
     } else {
         run(
             global,
             locals,
-            inputseqs,
+            input_seqs,
             aastream.map(SortingBuffer::new),
             metastream.map(SortingBuffer::new),
             gffstream.map(SortingBuffer::new),
             dnastream.map(SortingBuffer::new),
-            matches.value_of("complete").unwrap() == "1",
-            matches.is_present("formatted"),
-            usize::from_str_radix(matches.value_of("thread-num").unwrap(), 10)?,
+            matches.get_one::<String>("complete").map(|s| s == "1").unwrap_or(false),
+            matches.contains_id("formatted"),
+            matches.get_one::<String>("thread-num").and_then(|s| s.parse::<usize>().ok()).unwrap_or(1),
         )?;
     }
 
@@ -236,16 +234,14 @@ fn run<R: Read + Send, W: WritingBuffer + Send>(
             for record in recordvec {
                 let fasta::OwnedRecord { mut head, seq } = record?;
                 head = head.into_iter().take_while(u8::is_ascii_graphic).collect();
-                let nseq: Vec<Nuc> = seq
-                    .iter()
-                    .map(u8::to_ascii_uppercase)
-                    .map(Nuc::from)
-                    .collect();
-                let read_prediction = if nseq.is_empty() {
-                    gene::ReadPrediction::new(head)
-                } else {
-                    viterbi(&global, &locals, head, nseq, whole_genome)
-                };
+                let nseq: Vec<Nuc> = seq.into_iter().map(Nuc::from).collect();
+                let read_prediction = viterbi(
+                    &global,
+                    &locals[count_cg_content(&nseq)],
+                    head,
+                    nseq,
+                    whole_genome,
+                );
                 if meta_buffer.is_some() {
                     read_prediction.meta(&mut metabuf)?;
                 }
